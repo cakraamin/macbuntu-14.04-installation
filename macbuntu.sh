3b26be21c99f93e0d883d3ce60f2ef2c5b70c66e
Ranjith Siji <ranjith.sajeev@gmail.com>
2014-04-24 13:00:51 +0530

    Update macbuntu.sh
    Adding the Ubuntu 14.04 specific commands to the script

--- macbuntu.sh
+++ macbuntu.sh
@@ -1,45 +1,82 @@
-#Docky
-#sudo add-apt-repository ppa:docky-core/ppa
-#sudo apt-get update
-#sudo apt-get install docky
+#!/bin/bash
+echo 'Hi, $USER!'
+echo'Welcome to Macbuntu Script 14.04'
 
-#Latest Cairo Dock
-sudo add-apt-repository ppa:cairo-dock-team/ppa
+echo 'Downloading Mac Wallpapers ....'
+wget -O mac-wallpapers.zip http://drive.noobslab.com/data/Mac-13.10/MBuntu-Wallpapers.zip
+
+echo '=================================='
+echo 'Installing Unity Tweak Tool...'
+sudo apt-get install unity-tweak-tool -y
+
+echo '=================================='
+echo 'Installing Docky, The Mac OS X Dock'
+sudo add-apt-repository ppa:docky-core/ppa -y
 sudo apt-get update
-sudo apt-get install cairo-dock cairo-dock-plug-ins
+sudo apt-get install docky -y
 
- #Remove Overlay Scrollbars
+echo '=================================='
+echo 'Downloading Docky Mac theme'
+wget -O Buyi-idock.rar http://drive.noobslab.com/data/Mac-14.04/Mac-OS-Lion%28Docky%29.tar
+
+echo '=================================='
+echo 'Replace Ubuntu overlay scrollbars with normal scroll bars'
 gsettings set com.canonical.desktop.interface scrollbar-mode normal
 
-#indicator-synapse
-sudo add-apt-repository ppa:noobslab/apps -y
-sudo apt-get update
-sudo apt-get install indicator-synapse -y
 
-#Apple Logo in Launcher
-wget -O launcher_bfb.png http://goo.gl/7AIQCA
+echo '=================================='
+echo 'Replace ‘Ubuntu Desktop‘ text with ‘Mac OS X‘ on the top Panel'
+wget -O Mac.po http://drive.noobslab.com/data/Mac-14.04/change-name-on-panel/mac.po
+sudo msgfmt -o /usr/share/locale/en/LC_MESSAGES/unity.mo Mac.po;rm Mac.po;
+
+
+echo '=================================='
+echo 'Replace the Ubuntu logo with Apple logo on Launcher'
+wget -O launcher_bfb.png http://drive.noobslab.com/data/Mac-14.04/launcher-logo/apple/launcher_bfb.png
 sudo mv launcher_bfb.png /usr/share/unity/icons/
 
-#Apply MBuntu Splash
+
+echo '=================================='
+echo 'Apply Mac OS X Splash Screen to Ubuntu 14.04'
 sudo add-apt-repository ppa:noobslab/themes -y
 sudo apt-get update
-sudo apt-get install mbuntu-bscreen-v2 -y
+sudo apt-get install mbuntu-bscreen-v3 -y
 
-#Mac OS X Lion Theme, Icons and cursors:
-sudo add-apt-repository ppa:noobslab/themes -y
-sudo apt-get update
-sudo apt-get install mac-icons-v2-noobslab -y
-sudo apt-get install mac-ithemes-v2-noobslab -y
 
-#Disable Crash reports:
+echo '=================================='
+echo 'Apply Mac OS X Login Screen to Ubuntu 14.04'
+sudo apt-get install mbuntu-lightdm-v3 -y
+
+
+echo '=================================='
+echo 'Install Mac OS X theme for Ubuntu 14.04'
+sudo apt-get install mac-ithemes-v3 -y
+sudo apt-get install mac-icons-v3 -y
+
+
+echo '=================================='
+echo 'Disable Crash Reports on Ubuntu 14.04'
 sudo sed -i "s/enabled=1/enabled=0/g" '/etc/default/apport'
 
-#Unity/Gnome Tweak Tool to change Themes, Icons
-sudo apt-get install unity-tweak-tool -y
-sudo apt-get install gnome-tweak-tool -y
 
-#Remove White Dots and Ubuntu Logo from Login Screen:
+echo '=================================='
+echo 'Install Monochrome icons for Libreoffice in Ubuntu 14.04'
+sudo apt-get install libreoffice-style-sifr -y
+echo 'Change Libreoffice Icon theme on Libreoffice > Tools > Options > View > Icon Size and Style to Sifr'
+
+echo '=================================='
+echo 'Remove White Dots and Ubuntu Logo from Lock Screen'
 sudo xhost +SI:localuser:lightdm
 sudo su lightdm -s /bin/bash
-gsettings set com.canonical.unity-greeter draw-grid false && exit
+gsettings set com.canonical.unity-greeter draw-grid false;exit
 sudo mv /usr/share/unity-greeter/logo.png /usr/share/unity-greeter/logo.png.backup
+
+echo 'Macbuntu Script Completed Successfully .....'
+echo 'Open Unity Tweak Tool. Change Theme to Mbuntu-ac, Icons to Mbuntu-osx, Cursor to Mac-cursors'
+echo 'Also Install Docky theme by selecting Buyi-idock.rar in Docky Settings'
+echo 'Details are on http://smashingweb.info/mac-os-x-theme-for-ubuntu-14-04-macbuntu-transformation-pack/'
+echo 'Hide Unity Launcher on Settings > Appearance -> Behavior and turn off.'
+
+echo 'Visit http://smashingweb.info'
+echo 'Enjoy Mac Ubuntu 14.04'
+echo 'Restart Ubuntu'
